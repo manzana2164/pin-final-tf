7ca728e55881f78eaa9152492a2c667cea146eac
manzana2164 <emiliopascutti2164@gmail.com>
2025-03-04 04:01:19 +0000

    Update de version de eks de 1.22 a 1.32 para la region us-east-1

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 18.0"
 
   cluster_name    = "eks-cluster-mundose"
-  cluster_version = "1.22"
+  cluster_version = "1.32"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
