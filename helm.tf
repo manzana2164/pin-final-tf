554fd8c920945be5835bd7b1a576c92e636927da
Emilio Pascutti <111916983+manzana2164@users.noreply.github.com>
2025-03-11 21:49:27 -0300

    Update helm.tf for nginx deployment

--- helm.tf
+++ helm.tf
@@ -61,6 +61,56 @@ resource "helm_release" "grafana" {
   }
 }
 
+resource "kubernetes_deployment" "nginx_logs_deployment" {
+  metadata {
+    name      = "nginx-logs-deployment"
+    namespace = "default"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "256Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_service" "nginx_logs_service" {
   metadata {
     name      = "nginx-logs-service"
